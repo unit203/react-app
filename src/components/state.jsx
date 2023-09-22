ace0c4e065f6dce6aedfd721381dc3791fb48b6c
unit203github <shop2623@yandex.ru>
2023-09-22 09:47:23 +0300

    Фетч добавлен

--- src/components/state.jsx
+++ src/components/state.jsx
@@ -7,13 +7,15 @@ const user = {
   about: "Информация о себе, много букв или может быть и нет.",
 };
 
-const users = {
-  0: {id: 65, name: "pink", lastname: "floid", email: "floid@mail.com", avatar: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D&w=300&q=80",},
-  1: {id: 34, name: "agata", lastname: "kristi", email: "kristi@ma.com", avatar: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlciUyMHBlcnNvbmF8ZW58MHx8MHx8fDA%3D&w=300&q=80",},
-  2: {id: 33, name: "Marsal", lastname: "Jukov", email: "jukov@mj.com", avatar: "https://images.unsplash.com/photo-1573497019940-1c28c88b4f3e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dXNlciUyMHBlcnNvbmF8ZW58MHx8MHx8fDA%3D&w=300&q=80",},
-  3: {id: 22, name: "Slevin", lastname: "Celevra", email: "celevra@move.com", avatar: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D&w=300&q=80",},
-  4: {id: 11, name: "ernest", lastname: "FonClausevic", email: "fonclausevic@del.com", avatar: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlciUyMHBlcnNvbmF8ZW58MHx8MHx8fDA%3D&w=300&q=80",},
-}
+let users=[];
+
+// const users = {
+//   0: {id: 65, name: "pink", lastname: "floid", email: "floid@mail.com", avatar: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D&w=300&q=80",},
+//   1: {id: 34, name: "agata", lastname: "kristi", email: "kristi@ma.com", avatar: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlciUyMHBlcnNvbmF8ZW58MHx8MHx8fDA%3D&w=300&q=80",},
+//   2: {id: 33, name: "Marsal", lastname: "Jukov", email: "jukov@mj.com", avatar: "https://images.unsplash.com/photo-1573497019940-1c28c88b4f3e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dXNlciUyMHBlcnNvbmF8ZW58MHx8MHx8fDA%3D&w=300&q=80",},
+//   3: {id: 22, name: "Slevin", lastname: "Celevra", email: "celevra@move.com", avatar: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D&w=300&q=80",},
+//   4: {id: 11, name: "ernest", lastname: "FonClausevic", email: "fonclausevic@del.com", avatar: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlciUyMHBlcnNvbmF8ZW58MHx8MHx8fDA%3D&w=300&q=80",},
+// }
 
 export function getUser(userId) {
   console.log(users);
@@ -26,6 +28,9 @@ export function getUser(userId) {
   return user;
 }
 
-export function getUsers() {
+export async function getUsers() {
+  let response = await fetch("https://www.user.lh1.in/getUsers");
+  users = await response.json();
+  console.log(users);
   return users;
 }
